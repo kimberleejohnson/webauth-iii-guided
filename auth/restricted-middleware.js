c14efa869367c62d8f9dda147c0bf029e78d7683
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-12 10:11:20 -0700

    Addedrestricted middleware that checks for a token

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,26 +1,33 @@
-const bcrypt = require('bcryptjs');
+// Can remove bcrypt and Users library because no longer need 
 
-const Users = require('../users/users-model.js');
+// Require jwt library so we can verify token
+const jwt = require('jsonwebtoken');
+
+// Require secrets so secrets can also be decoded
+const secrets = require('../config/secrets.js'); 
+
 
 module.exports = (req, res, next) => {
 
   // Eventually we want to change headers to check for tokens 
-  const { username, password } = req.headers;
+  // Not reading from headers so can delete 
+  // Token often attached to headers 
+  const token = req.headers.authorization; 
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  // Check if there is a token 
+  if (token) {
+
+    // Verify method verifies token, decodes secret, and also pass in a function
+    jwt.verify(token, secrets.jwtSecret, (err, decodeToken) => {
+      if(err) {
+        // Invalid token 
+        res.status(401).json({ message: 'Invalid Token' });
+      } else {
+        // Valid token 
+        next(); 
+      }
+    }); 
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: 'No token provided' });
   }
 };
